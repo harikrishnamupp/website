1a446548b0d908199057a67df9adf6f02d7e6347
Kevin Miller <keveemiller@gmail.com>
2020-11-19 10:23:04 -0800

    chore: Add algolia search

--- src/pages/data/longtermcare/map.js
+++ src/pages/data/longtermcare/map.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState, useRef } from 'react'
+import algoliasearch from 'algoliasearch'
 import { graphql, useStaticQuery } from 'gatsby'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-
+import { Form, Input } from '~components/common/form'
+import { Row, Col } from '~components/common/grid'
 import Layout from '~components/layout'
 
 const Map = ({ center, zoom }) => {
@@ -17,6 +20,13 @@ const Map = ({ center, zoom }) => {
       }
     `,
   )
+  const [facilities, setFacilities] = useState(false)
+  const [query, setQuery] = useState(false)
+  const client = algoliasearch(
+    process.env.GATSBY_ALGOLIA_APP_ID,
+    process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  )
+  const index = client.initIndex('test_ltc_geo_facilities')
   if (typeof window === 'undefined') {
     return null
   }
@@ -60,12 +70,56 @@ const Map = ({ center, zoom }) => {
     }
   }, [])
 
-  return <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
-}
+  useEffect(() => {
+    if (!facilities || !facilities.length) {
+      return
+    }
+    facilities.forEach(facility => {
+      if (typeof facility._geoloc === 'undefined') {
+        return
+      }
+      const marker = new mapboxgl.Marker()
+        .setLngLat([facility._geoloc.lng, facility._geoloc.lat])
+        .addTo(mapRef.current)
+      console.log(marker)
+    })
+  }, [facilities])
 
-const LTCMapPage = () => {
   return (
-    <Layout title="Sample LTC Map">
+    <>
+      <Form
+        onSubmit={event => {
+          event.preventDefault()
+          console.log(query)
+          index
+            .search('', {
+              aroundLatLng: '37.5706625, -81.4748938',
+              aroundRadius: 1000000,
+              distinct: true,
+            })
+            .then(({ hits }) => {
+              setFacilities(hits)
+            })
+        }}
+        noMargin
+      >
+        <Row>
+          <Col width={[3, 3, 8]}>
+            <Input
+              type="text"
+              label="Search facilities"
+              placeholder="Enter a city or zip code"
+              hideLabel
+              onChange={event => {
+                setQuery(event.target.value)
+              }}
+            />
+          </Col>
+          <Col width={[1, 3, 4]}>
+            <button type="submit">Search</button>
+          </Col>
+        </Row>
+      </Form>
       <div
         style={{
           position: 'relative',
@@ -74,10 +128,16 @@ const LTCMapPage = () => {
           height: 500,
         }}
       >
-        <Map center={[-97, 38]} zoom={3} />
+        <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
       </div>
-    </Layout>
+    </>
   )
 }
+
+const LTCMapPage = () => (
+  <Layout title="Sample LTC Map">
+    <Map center={[-97, 38]} zoom={3} />
+  </Layout>
+)
 
 export default LTCMapPage
